66729659844574058626fd1b65b87878e04fd482
hieuso <hieuphso@gmail.com>
2024-08-07 23:34:39 -0400

    authorities and roles

--- src/main/java/spring/security/learning/learningspringsecurity/config/SecurityConfig.java
+++ src/main/java/spring/security/learning/learningspringsecurity/config/SecurityConfig.java
@@ -1,28 +1,45 @@
 package spring.security.learning.learningspringsecurity.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-
-import lombok.AllArgsConstructor;
-import spring.security.learning.learningspringsecurity.config.filters.APIKeyFilter;
 
 @Configuration
-@AllArgsConstructor
 public class SecurityConfig {
 
-	private String key = "test";
-
 	@Bean
-	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        return http.httpBasic()
-        		.and().addFilterBefore(new APIKeyFilter(key), BasicAuthenticationFilter.class)
-        		.authorizeRequests().anyRequest().authenticated().and()
-        		.build();
-    }
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+//		return httpSecurity.httpBasic().and().authorizeHttpRequests().anyRequest().permitAll().and().build();
+		return httpSecurity.httpBasic()
+				.and()
+//				.authorizeRequests()
+//				.securityMatcher("/demo")httpSecurity.autho
+//				.anyRequest()
+//				.hasAnyRole("ADMIN")
+				.authorizeHttpRequests(auth -> 
+						auth.requestMatchers("/demo").hasAnyAuthority("read").anyRequest().authenticated())
+				.build();
+	}
 	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		InMemoryUserDetailsManager uds = new InMemoryUserDetailsManager();
+		User aUser = (User) User.withUsername("bill").password(passwordEncoder().encode("123456")).roles("ADMIN").authorities("read").build();
+		uds.createUser(aUser);
+		return uds;
+	}
 	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 	
 }
--- src/main/java/spring/security/learning/learningspringsecurity/controller/DemoController.java
+++ src/main/java/spring/security/learning/learningspringsecurity/controller/DemoController.java
@@ -1,15 +1,19 @@
 package spring.security.learning.learningspringsecurity.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
-	@GetMapping("/demo")
-	public String demo() {
-		return "Demo";
-	}
-	
+  @GetMapping("/demo")
+  public String demo() {
+    return "Demo!";
+  }
+
+  @GetMapping("/hello")
+  public String hello() {
+    return "Hello!";
+  }
+
 }
